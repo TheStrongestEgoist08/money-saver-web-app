3cd616ab29eed3e00412d03ce133c634b06c268f
TheStrongestEgoist08 <bigalcarlmichael799@gmail.com>
2026-04-07 09:58:36 +0800

    update

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,44 @@
+{{--
+    @props(['disabled' => false])
+    <input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+--}}
+
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+@php
+    $type = $attributes->get('type', 'text');
+@endphp
+
+<div
+    x-data="{ show: false }"
+    class="relative"
+>
+    <input
+        :type="show ? 'text' : '{{ $type }}'"
+        @disabled($disabled)
+        {{ $attributes->merge([
+            'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' . ($type === 'password' ? 'pr-10' : '')
+        ]) }}
+    >
+
+    @if($type === 'password')
+        <!-- Eye Button -->
+        <button
+            type="button"
+            @click="show = !show"
+            class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-500"
+        >
+            <!-- Eye -->
+            <svg x-show="!show" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
+                <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0"/>
+                <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7"/>
+            </svg>
+
+            <!-- Eye Slash -->
+            <svg x-show="show" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye-slash-fill" viewBox="0 0 16 16">
+                <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z"/>
+                <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z"/>
+            </svg>
+        </button>
+    @endif
+</div>
