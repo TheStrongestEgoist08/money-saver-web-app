3b9b77a35762eaaacfb27a4bc362825161d68f52
TheStrongestEgoist08 <bigalcarlmichael799@gmail.com>
2026-05-19 21:53:33 +0800

    bug fixed for transaction

--- app/Http/Controllers/User/TransactionController.php
+++ app/Http/Controllers/User/TransactionController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
     public function index()
     {
-        $transactions = Transaction::latest()->paginate(10);
+        $transactions = Transaction::where('user_id', Auth::id())
+            ->latest()
+            ->paginate(10);
 
         $transactions->setCollection(
             $transactions->getCollection()->groupBy([
